b23c5310b691ec00c06e3516783276a0950c43f1
Ginelle Plaisir <plaisirginelle@gmail.com>
2020-07-05 23:31:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,4 +8,12 @@ class App < Sinatra::Base
    get '/name' do
   "My name is #{}!"
   end
+  
+   get '/hometown' do
+  "My hometown is #{}!"
+  end
+  
+   get '/favorite-song' do
+  "My favorite song is #{}!"
+  end
 end
